0b105434c9480a6512abf600f4da4a8876fb8552
Yevheniia Alikberova <e.alikberova@outlook.com>
2026-09-12 14:52:06 +0200

    fix JwtConfig__SigningKey env variable is not set in local docker compose

--- TextToSpeech.Web/nginx/certs/generate-cert.ps1
+++
@@ -1,6 +0,0 @@
-# powershell -ExecutionPolicy Bypass -File ./generate-cert.ps1
-
-& "C:\Program Files\Git\usr\bin\openssl.exe" req -x509 -newkey rsa:2048 -nodes `
-  -keyout "./privkey.pem" `
-  -out "./fullchain.pem" `
-  -days 365 -subj "/CN=localhost"
